f5b2f06a06718bbc76c8719a21af8e165a1c95b6
Ajimotokan Johnson <ajimotokanjohnson@gmail.com>
2022-10-14 15:31:15 +0100

    Update Project - Part 1: Sentiment Classifier.py

--- Project - Part 1: Sentiment Classifier.py
+++ Project - Part 1: Sentiment Classifier.py
@@ -16,51 +16,26 @@ with open("negative_words.txt") as pos_f:
         if lin[0] != ';' and lin[0] != '\n':
             negative_words.append(lin.strip())
 
-def strip_punctuation(string):
-    for i in punctuation_chars:
-        string = string.replace(i, "")
-    return string
+outfile = open("resulting_data.csv", "w")
+outfile.write('Number of Retweets, Number of Replies, Positive Score, Negative Score, Net Score')
+outfile.write('\n')
 
-def get_pos(string):
-    count_positive = 0
-    #Convert all the words to lower case and get rid of punctuation
-    string = strip_punctuation(string)
-    string = string.lower()
-    new_lst = string.split(" ")
-    #Count positive words
-    for word in new_lst:
+myfile = open('project_twitter_data.csv', 'r')
+rows = myfile.readlines()[1:]
+for line in rows:
+    words = line.split()
+    numbers = words[-1]
+    twrt = numbers.split(',')
+    print ('retweets: ', twrt[1], 'replies: ', twrt[2])
+    pos_sco = 0
+    neg_sco = 0
+    for word in words:
         if word in positive_words:
-            count_positive += 1
-    return count_positive
-
-def get_neg(string):
-    count_negative = 0
-    #Convert all the words to lower case and get rid of punctuation
-    string = strip_punctuation(string)
-    string = string.lower()
-    new_lst = string.split(" ")
-    #Count negative words
-    for word in new_lst:
+            pos_sco = pos_sco + 1
         if word in negative_words:
-            count_negative += 1
-    return count_negative
-
-
-
-def writeDataInFile(resultingData):
-    resultingData.write("Number of Retweets, Number of Replies, Positive Score, Negative Score, Net Score")
-    resultingData.write("\n")
-    
-    lines = projectTwitterData.readlines()
-    header = lines[0]
-    
-    for word in lines[1:]:
-        lst = word.strip().split(',')
-        resultingData.write("{}, {}, {}, {}, {}".format(lst[1], lst[2], get_pos(lst[0]), get_neg(lst[0]), (get_pos(lst[0])-get_neg(lst[0]))))    
-        resultingData.write("\n")
-
-        
-
-writeDataInFile(resultingData)
-projectTwitterData.close()
-resultingData.close()
+            neg_sco = neg_sco + 1
+    net_sco = pos_sco - neg_sco
+    print ('positive words: ', pos_sco, 'negative words: ', neg_sco, 'Net score: ', net_sco )
+    row_string = '{}, {}, {}, {}, {}'.format(twrt[1], twrt[2], pos_sco, neg_sco, net_sco)
+    outfile.write(row_string)
+    outfile.write('\n')
